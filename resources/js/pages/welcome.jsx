8731e8ef2abc6cf1bb7b0d3c28c7c40dd482fbbc
yannicktch <yannicktchoukouani@gmail.com>
2025-06-02 10:35:03 +0200

    new header

--- resources/js/pages/welcome.jsx
+++ resources/js/pages/welcome.jsx
@@ -1,43 +1,13 @@
-// import { Head, Link, usePage } from '@inertiajs/react';
-// import { Fragment, useState } from 'react';
-// ;
-// // import { Bars3Icon, MagnifyingGlassIcon, ShoppingCartIcon, UserIcon, XMarkIcon } from '@heroicons/react/24/outline';
-// import { ChevronDownIcon } from '@heroicons/react/20/solid';
-// import Header from '@/Components/Header';
-// import Footer from '@/Components/Footer';
-// //import { Bars3Icon, MagnifyingGlassIcon, ShoppingCartIcon, UserIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
-// export default function Welcome() {
-//   const { auth } = usePage().props; // Correctly placed usePage() hook
-
-//   const currencies = ['EUR', 'CAD', 'USD', 'AUD', 'GBP'];
-
- 
-
-//   const [open, setOpen] = useState(false);
-
-//   return (
-//     <>
       <Head title="ucisee.shop">
 //         <link rel="preconnect" href="https://fonts.bunny.net" />
 //         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 //       </Head>
 
-//       <Header />
-
-
-      
-
-
-//       <Footer />
-//     </>
-//   );
-// }
 
 import { Head, Link } from '@inertiajs/react';
 import Footer from '../components/footer';
 import Header from '../components/header';
-import Secondheader from '../components/secondheader';
 // Components :
 
 // Product images : 
@@ -114,8 +84,6 @@ export default function Welcome({ phpVersion }) {
             </div>
 
             <Footer />
-
-            <Secondheader />
         </>
     );
 }
