80108a10436d3b6fdf1375459cb04a753134ae88
yannicktch <yannicktchoukouani@gmail.com>
2025-06-05 11:21:24 +0200

    new footer

--- resources/js/components/footer.jsx
+++ resources/js/components/footer.jsx
@@ -1,16 +1,37 @@
+import logo from '../ASSETS/images/LOGO-CARTOON.png';
+
+
 export default function Footer() {
   return (
-    <footer className="bg-white dark:bg-gray-900">
-      <div className="max-w-screen-xl mx-auto px-4 py-8">
-        <div className="text-center text-gray-500 dark:text-gray-400">
-          <p className="text-sm">
-            &copy; {new Date().getFullYear()} ucisee.shop. All rights reserved.
-          </p>
-          <p className="text-xs mt-2">
-            Made with ❤️ by ucisee
-          </p>
+    
+
+<footer class="bg-white">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="../welcome.jsx" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src={logo} class="h-22" alt="share the vision." />
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-black sm:mb-0">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Shop</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Contact</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Login</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">About Us</a>
+                </li>
+            </ul>
         </div>
-      </div>
-    </footer>
+        <hr class="my-6 border-black sm:mx-auto lg:my-8" />
+        <span class="block text-sm text-black sm:text-center">© 2025 <a href="https://flowbite.com/" class="hover:underline">ucisee™</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+
+
+
   );
 }
